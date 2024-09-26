ec17461fd9c8e46c2f9d1ace8812614f29cbd3c3
Anna V <annreznik134@gmail.com>
2024-09-26 20:00:02 +0300

    Added negative UI test

--- src/test/java/SimpleUITest.java
+++ src/test/java/SimpleUITest.java
@@ -8,6 +8,51 @@ import static com.codeborne.selenide.Selenide.element;
 
 public class SimpleUITest {
 
+
+    @Test
+    public void userCreateAccount(){
+        //Подготовка
+        Selenide.open("https://parabank.parasoft.com/parabank/register.htm");
+
+        //Логика теста
+        SelenideElement firstName = element(Selectors.byId("customer.firstName"));
+        firstName.sendKeys("Anna");
+
+        SelenideElement lastName = element(Selectors.byId("customer.lastName"));
+        lastName.sendKeys("Mikhailova");
+
+        SelenideElement address = element(Selectors.byId("customer.address.street"));
+        address.sendKeys("Green street");
+
+        SelenideElement city = element(Selectors.byId("customer.address.street"));
+        city.sendKeys("Saint-P");
+
+        SelenideElement state = element(Selectors.byId("customer.address.state"));
+        state.sendKeys("No");
+
+        SelenideElement zipCode = element((Selectors.byId("customer.address.zipCode")));
+        zipCode.sendKeys("1234567");
+
+        SelenideElement phoneNumber = element((Selectors.byId("customer.phoneNumber")));
+        phoneNumber.sendKeys("7654321");
+
+        SelenideElement ssn = element((Selectors.byId("customer.ssn")));
+       ssn.sendKeys("7645678");
+
+        SelenideElement username = element((Selectors.byId("customer.username")));
+        username.sendKeys("Anna");
+
+        SelenideElement password = element((Selectors.byId("customer.password")));
+        password.sendKeys("dsfhdkjfh");
+
+        SelenideElement passwordConfirm = element((Selectors.byId("repeatedPassword")));
+        passwordConfirm.sendKeys("dsfhdkjfh");
+
+
+
+
+    }
+
     @Test
     public void userCantCreateAccountOnlyWithName(){
         //Подготовка
